0db8bfed62bd034b6fd40fc454595b95a190ff0a
Uwe Luethy <uwe_luethy@hotmail.com>
2025-10-13 15:00:21 +0200

    Update OnboardingScriptARConLinux

--- OnboardingScript8973.sh
+++ OnboardingScript8973.sh
@@ -1,22 +1,16 @@
 
 export subscriptionId="c5365ae4-76ab-4929-907e-49d8456be106";
-export resourceGroup="cusotmer-workloads";
-export tenantId="cfb448a9-6c04-45ec-b313-ae97e9636ba7";
+export resourceGroup="rg-onpremworkloads";
+export tenantId="37a7976b-efcd-4fcf-a65e-504964db0447";
 export location="eastus";
 export authType="token";
-export correlationId="d607f70f-8f85-47ac-b8b9-ca247c1b1140";
+export correlationId="a78002e1-7d2f-4d9a-85cd-3cdcb7477460";
 export cloud="AzureCloud";
-
-
-# Download the installation package
 LINUX_INSTALL_SCRIPT="/tmp/install_linux_azcmagent.sh"
 if [ -f "$LINUX_INSTALL_SCRIPT" ]; then rm -f "$LINUX_INSTALL_SCRIPT"; fi;
 output=$(wget https://gbl.his.arc.azure.com/azcmagent-linux -O "$LINUX_INSTALL_SCRIPT" 2>&1);
 if [ $? != 0 ]; then wget -qO- --method=PUT --body-data="{\"subscriptionId\":\"$subscriptionId\",\"resourceGroup\":\"$resourceGroup\",\"tenantId\":\"$tenantId\",\"location\":\"$location\",\"correlationId\":\"$correlationId\",\"authType\":\"$authType\",\"operation\":\"onboarding\",\"messageType\":\"DownloadScriptFailed\",\"message\":\"$output\"}" "https://gbl.his.arc.azure.com/log" &> /dev/null || true; fi;
 echo "$output";
-
-# Install the hybrid agent
 bash "$LINUX_INSTALL_SCRIPT";
-
-# Run connect command
-sudo azcmagent connect --resource-group "$resourceGroup" --tenant-id "$tenantId" --location "$location" --subscription-id "$subscriptionId" --cloud "$cloud" --tags 'Datacenter=ROG,City=Gottmadingen,CountryOrRegion=Germany,ArcSQLServerExtensionDeployment=Disabled' --correlation-id "$correlationId";
+sleep 5;
+sudo azcmagent connect --resource-group "$resourceGroup" --tenant-id "$tenantId" --location "$location" --subscription-id "$subscriptionId" --cloud "$cloud" --tags 'ArcSQLServerExtensionDeployment=Disabled' --correlation-id "$correlationId";
